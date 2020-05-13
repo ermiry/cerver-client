f4ee1c93cf6ade1a245ca01e4acae868d93e389b
ermiry <erick_salas@ermiry.com>
2020-05-12 22:50:18 -0500

    Refactor client version source files

--- include/client/version.h
+++ include/client/version.h
@@ -3,8 +3,8 @@
 
 #define CERVER_CLIENT_VERSION                   "1.1"
 #define CERVER_CLIENT_VERSION_NAME              "Release 1.1"
-#define CERVER_CLIENT_VERSION_DATE			    "11/05/2020"
-#define CERVER_CLIENT_VERSION_TIME			    "06:01 CST"
+#define CERVER_CLIENT_VERSION_DATE			    "12/05/2020"
+#define CERVER_CLIENT_VERSION_TIME			    "22:47 CST"
 #define CERVER_CLIENT_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver client version information 
--- src/client/version.c
+++ src/client/version.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#include "version.h"
+#include "client/version.h"
 
 // print full version information 
 void cerver_client_version_print_full (void) {
 
-    printf ("\n\nCengine Version: %s\n", CERVER_CLIENT_VERSION_NAME);
+    printf ("\n\nCerver Client Version: %s\n", CERVER_CLIENT_VERSION_NAME);
     printf ("Release Date & time: %s - %s\n", CERVER_CLIENT_VERSION_DATE, CERVER_CLIENT_VERSION_TIME);
     printf ("Author: %s\n\n", CERVER_CLIENT_VERSION_AUTHOR);
 
