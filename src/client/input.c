2c80b05c6798bb42eeaae562dbf67194cd250b76
ermiry <erick_salas@ermiry.com>
2020-06-23 03:08:28 -0500

    Small fix in input_get_line ()

--- src/client/input.c
+++ src/client/input.c
@@ -5,69 +5,70 @@
 
 void input_clean_stdin (void) {
 
-    int c = 0;
-    do {
-        c = getchar ();
-    } while (c != '\n' && c != EOF);
+	int c = 0;
+	do {
+		c = getchar ();
+	} while (c != '\n' && c != EOF);
 
 }
 
 // returns a newly allocated c string
 char *input_get_line (void) {
 
-    size_t lenmax = 128, len = lenmax;
-    char *line = malloc (lenmax), *linep = line;
-    int c = 0;
+	size_t lenmax = 128, len = lenmax;
+	char *line = malloc (lenmax), *linep = line;
 
-    if (line) {
-        for (;;) {
-            c = fgetc (stdin);
+	if (line) {
+		int c = 0;
 
-            if (c == EOF) break;
+		for (;;) {
+			c = fgetc (stdin);
 
-            if (--len == 0) {
-                len = lenmax;
-                char * linen = realloc (linep, lenmax *= 2);
+			if (c == EOF || c == '\n') break;
 
-                if(linen == NULL) {
-                    free (linep);
-                    return NULL;
-                }
+			if (--len == 0) {
+				len = lenmax;
+				char * linen = realloc (linep, lenmax *= 2);
 
-                line = linen + (line - linep);
-                linep = linen;
-            }
+				if(linen == NULL) {
+					free (linep);
+					return NULL;
+				}
 
-            if ((*line++ = c) == '\n') break;
-        }
+				line = linen + (line - linep);
+				linep = linen;
+			}
 
-        *line = '\0';
-    }
+			if ((*line++ = c) == '\n') break;
+		}
 
-    return linep;
+		*line = '\0';
+	}
+
+	return linep;
 
 }
 
 unsigned int input_password (char *password) {
 
-    unsigned int retval = 1;
+	unsigned int retval = 1;
 
 	struct termios old = { 0 }, new = { 0 };
 
 	/* Turn echoing off and fail if we can't. */
 	if (!tcgetattr (fileno (stdin), &old)) {
-        new = old;
-        new.c_lflag &= ~ECHO;
-        if (!tcsetattr (fileno (stdin), TCSAFLUSH, &new) != 0) {
-            printf ("Enter password: ");
-            scanf ("%128s", password);
+		new = old;
+		new.c_lflag &= ~ECHO;
+		if (!tcsetattr (fileno (stdin), TCSAFLUSH, &new) != 0) {
+			printf ("Enter password: ");
+			scanf ("%128s", password);
 
-            /* Restore terminal. */
-            (void) tcsetattr (fileno (stdin), TCSAFLUSH, &old);
+			/* Restore terminal. */
+			(void) tcsetattr (fileno (stdin), TCSAFLUSH, &old);
 
-            retval = 0;
-        }
-    }
+			retval = 0;
+		}
+	}
 
 	return retval;
 
