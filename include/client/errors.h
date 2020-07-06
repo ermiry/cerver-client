f83fb7c2cbcfafaa514410a62fea3b1371dd8bd7
ermiry <erick_salas@ermiry.com>
2020-07-06 17:20:14 -0500

    Working in new client errors

--- include/client/errors.h
+++ include/client/errors.h
@@ -1,54 +1,82 @@
 #ifndef _CLIENT_ERRORS_H_
 #define _CLIENT_ERRORS_H_
 
+#include <stdbool.h>
+
+#include "client/types/types.h"
 #include "client/types/string.h"
 
 #include "client/packets.h"
 
+struct _Client;
+struct _Connection;
 struct _Packet;
 
-typedef enum ErrorType {
+typedef enum ClientErrorType {
 
-    ERR_NONE                    = 0,
+	ERR_NONE                    = 0,
 
-    // internal server error, like no memory
-    ERR_CERVER_ERROR            = 1,   
+	// internal server error, like no memory
+	ERR_CERVER_ERROR            = 1,   
 
-    ERR_CREATE_LOBBY            = 2,
-    ERR_JOIN_LOBBY              = 3,
-    ERR_LEAVE_LOBBY             = 4,
-    ERR_FIND_LOBBY              = 5,
+	ERR_CREATE_LOBBY            = 2,
+	ERR_JOIN_LOBBY              = 3,
+	ERR_LEAVE_LOBBY             = 4,
+	ERR_FIND_LOBBY              = 5,
 
-    ERR_GAME_INIT               = 6,
-    ERR_GAME_START              = 7,
+	ERR_GAME_INIT               = 6,
+	ERR_GAME_START              = 7,
 
-    ERR_FAILED_AUTH             = 8,
+	ERR_FAILED_AUTH             = 8,
 
-} ErrorType;
+} ClientErrorType;
 
-// when a client error happens, it sets the appropaited msg (if any)
-// and an event is triggered
-typedef struct Error {
+typedef struct ClientError {
 
-    // TODO: maybe add time?
-    u32 error_type;
-    String *msg;
+	ClientErrorType error_type;
+	bool create_thread;                 // create a detachable thread to run action
+    bool drop_after_trigger;            // if we only want to trigger the event once
 
-} Error;
+	Action action;                      // the action to be triggered
+    void *action_args;                  // the action arguments
+    Action delete_action_args;          // how to get rid of the data
 
-extern Error *error_new (const char *msg);
-extern void error_delete (void *ptr);
+} ClientError;
+
+#pragma region data
+
+// structure that is passed to the user registered method
+typedef struct ClientErrorData {
+
+    struct _Client *client;
+    struct _Connection *connection;
+
+    void *action_args;                  // the action arguments set by the user
+
+} ClientErrorData;
+
+extern void client_error_data_delete (ClientErrorData *error_data);
+
+#pragma endregion
+
+#pragma region handler
 
 // handles error packets
 extern void error_packet_handler (struct _Packet *packet);
 
+#pragma endregion
+
+#pragma region serialization
+
 // serialized error data
 typedef struct SError {
 
-    time_t timestamp;
-    u32 error_type;
-    char msg[64];
+	time_t timestamp;
+	u32 error_type;
+	char msg[64];
 
 } SError;
+
+#pragma endregion
 
 #endif
--- src/client/errors.c
+++ src/client/errors.c
@@ -1,72 +1,98 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "client/types/types.h"
 #include "client/types/string.h"
 
+#include "client/collections/dlist.h"
+
+#include "client/client.h"
+#include "client/connection.h"
 #include "client/errors.h"
 #include "client/packets.h"
+
+#include "client/threads/thread.h"
 
 #include "client/utils/utils.h"
 #include "client/utils/log.h"
 
-Error *error_new (const char *msg) {
+#pragma region data
 
-    Error *error = (Error *) malloc (sizeof (Error));
-    if (error) {
-        memset (error, 0, sizeof (Error));
-        error->msg = msg ? str_new (msg) : NULL;
-    }
+static ClientErrorData *client_error_data_new (void) {
 
-    return error;
+	ClientErrorData *error_data = (ClientErrorData *) malloc (sizeof (ClientErrorData));
+	if (error_data) {
+		error_data->client = NULL;
+		error_data->connection = NULL;
+
+		error_data->action_args = NULL;
+	}
+
+	return error_data;
+
+}
+
+void client_error_data_delete (ClientErrorData *error_data) {
+
+	if (error_data) free (error_data);
 
 }
 
-void error_delete (void *ptr) {
+static ClientErrorData *client_error_data_create (Client *client, Connection *connection, void *args) {
 
-    if (ptr) {
-        Error *error = (Error *) ptr;
-        str_delete (error->msg);
-        free (error);
-    }
+	ClientErrorData *error_data = client_error_data_new ();
+	if (error_data) {
+		error_data->client = client;
+		error_data->connection = connection;
+		error_data->action_args = args;
+	}
+
+	return error_data;
 
 }
+
+#pragma endregion
+
+#pragma region handler
 
 // handles error packets
 void error_packet_handler (Packet *packet) {
 
-    if (packet) {
-        if (packet->data_size >= sizeof (SError)) {
-            char *end = (char *) packet->data;
-            SError *s_error = (SError *) end;
+	if (packet) {
+		if (packet->data_size >= sizeof (SError)) {
+			char *end = (char *) packet->data;
+			SError *s_error = (SError *) end;
 
-            switch (s_error->error_type) {
-                case ERR_CERVER_ERROR: break;
-                case ERR_CREATE_LOBBY: break;
-                case ERR_JOIN_LOBBY: break;
-                case ERR_LEAVE_LOBBY: break;
-                case ERR_FIND_LOBBY: break;
-                case ERR_GAME_INIT: break;
+			switch (s_error->error_type) {
+				case ERR_CERVER_ERROR: break;
+				case ERR_CREATE_LOBBY: break;
+				case ERR_JOIN_LOBBY: break;
+				case ERR_LEAVE_LOBBY: break;
+				case ERR_FIND_LOBBY: break;
+				case ERR_GAME_INIT: break;
 
-                case ERR_FAILED_AUTH: {
-                    // #ifdef CLIENT_DEBUG
-                    client_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, 
-                        c_string_create ("Failed to authenticate - %s", s_error->msg)); 
-                    // #endif
-                    // last_error.type = ERR_FAILED_AUTH;
-                    // memset (last_error.msg, 0, sizeof (last_error.msg));
-                    // strcpy (last_error.msg, error->msg);
-                    // if (pack_info->client->errorType == ERR_FAILED_AUTH)
-                        // pack_info->client->errorAction (pack_info->client->errorArgs);
-                }
-                    
-                break;
+				case ERR_FAILED_AUTH: {
+					// #ifdef CLIENT_DEBUG
+					client_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, 
+						c_string_create ("Failed to authenticate - %s", s_error->msg)); 
+					// #endif
+					// last_error.type = ERR_FAILED_AUTH;
+					// memset (last_error.msg, 0, sizeof (last_error.msg));
+					// strcpy (last_error.msg, error->msg);
+					// if (pack_info->client->errorType == ERR_FAILED_AUTH)
+						// pack_info->client->errorAction (pack_info->client->errorArgs);
+				}
+					
+				break;
 
-                default: 
-                    client_log_msg (stderr, LOG_WARNING, LOG_NO_TYPE, "Unknown error received from server."); 
-                    break;
-            }
-        }
-    }
+				default: 
+					client_log_msg (stderr, LOG_WARNING, LOG_NO_TYPE, "Unknown error received from server."); 
+					break;
+			}
+		}
+	}
 
 }
+
+#pragma endregion
