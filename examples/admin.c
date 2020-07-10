b43dcc8dd0643c561e79f4919ac471f635fc1df5
ermiry <erick_salas@ermiry.com>
2020-07-10 01:52:10 -0500

    Base admin example is working

--- examples/admin.c
+++ examples/admin.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <unistd.h>
 #include <signal.h>
@@ -7,7 +9,10 @@
 
 #include "client/client.h"
 #include "client/packets.h"
+#include "client/events.h"
+#include "client/errors.h"
 
+#include "client/utils/utils.h"
 #include "client/utils/log.h"
 
 typedef enum AppRequest {
@@ -21,7 +26,89 @@ static Connection *connection = NULL;
 
 static void app_handler (void *packet_ptr);
 
+#pragma region auth
+
+typedef struct Credentials {
+
+	char username[64];
+	char password[64];
+
+} Credentials;
+
+Credentials *credentials_new (const char *username, const char *password) {
+
+	Credentials *credentials = (Credentials *) malloc (sizeof (Credentials));
+	if (credentials) {
+		strncpy (credentials->username, username, 64);
+		strncpy (credentials->password, password, 64);
+	}
+
+	return credentials;
+
+}
+
+void credentials_delete (void *credentials_ptr) { if (credentials_ptr) free (credentials_ptr); }
+
+#pragma endregion
+
 #pragma region connect
+
+static void client_event_connection_close (void *client_event_data_ptr) {
+
+	if (client_event_data_ptr) {
+		ClientEventData *client_event_data = (ClientEventData *) client_event_data_ptr;
+
+		if (client_event_data->connection) {
+			char *status = c_string_create ("client_event_connection_close () - connection <%s> has been closed!",
+				client_event_data->connection->name->str);
+			if (status) {
+				client_log_warning (status);
+				free (status);
+			}
+		}
+
+		client_event_data_delete (client_event_data);
+	}
+
+}
+
+static void client_error_failed_auth (void *client_error_data_ptr) {
+
+	if (client_error_data_ptr) {
+		ClientErrorData *client_error_data = (ClientErrorData *) client_error_data_ptr;
+
+		if (client_error_data->connection) {
+			char *status = c_string_create ("client_error_failed_auth () - connection <%s> failed to authenticate!",
+				client_error_data->connection->name->str);
+			if (status) {
+				client_log_error (status);
+				free (status);
+			}
+		}
+
+		client_error_data_delete (client_error_data);
+	}
+
+}
+
+static void client_event_success_auth (void *client_event_data_ptr) {
+
+	if (client_event_data_ptr) {
+		ClientEventData *client_event_data = (ClientEventData *) client_event_data_ptr;
+
+		if (client_event_data->connection) {
+			char *status = c_string_create ("client_event_success_auth () - connection <%s> has been authenticated!",
+				client_event_data->connection->name->str);
+			if (status) {
+				client_log_success (status);
+				free (status);
+			}
+		}
+
+		client_event_data_delete (client_event_data);
+	}
+
+}
 
 static int cerver_connect (const char *ip, unsigned int port) {
 
@@ -34,10 +121,40 @@ static int cerver_connect (const char *ip, unsigned int port) {
 		if (client) {
 			client_set_app_handlers (client, app_handler, NULL);
 
+			client_event_register (
+				client, 
+				CLIENT_EVENT_CONNECTION_CLOSE, 
+				client_event_connection_close, NULL, NULL, 
+				false, false
+			);
+
+			client_error_register (
+				client,
+				CLIENT_ERR_FAILED_AUTH,
+				client_error_failed_auth, NULL, NULL,
+				false, false
+			);
+
+			client_event_register (
+				client,
+				CLIENT_EVENT_SUCCESS_AUTH,
+				client_event_success_auth, NULL, NULL,
+				false, false
+			);
+
 			connection = client_connection_create (client, ip, port, PROTOCOL_TCP, false);
 			if (connection) {
 				connection_set_name (connection, "main");
 				connection_set_max_sleep (connection, 30);
+
+				// auth configuration
+				Credentials *credentials = credentials_new ("ermiry", "hola12");
+				connection_set_auth_data (
+					connection, 
+					credentials, sizeof (Credentials), 
+					credentials_delete,
+					true
+				);
 
 				if (!client_connect_and_start (client, connection)) {
 					client_log_msg (stdout, LOG_SUCCESS, LOG_NO_TYPE, "Connected to cerver!");
@@ -157,19 +274,12 @@ int main (int argc, const char **argv) {
 	if (!cerver_connect ("127.0.0.1", 8007)) {
 		while (1) {
 			// send a test message every second
-			test_msg_send ();
-			test_msg_send ();
-			test_msg_send ();
+			// if (connection->authenticated) {
+				test_msg_send ();
+			// }
 
 			sleep (1);
 		}
-
-		// for (unsigned int i = 0; i < 5; i++) {
-		//     test_msg_send ();
-		//     sleep (1);
-		// }
-
-		// cerver_disconnect ();
 	}
 
 	return 0;
