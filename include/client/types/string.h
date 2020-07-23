82c18ad0f16ec2e565a642e0099987a87c9995d9
ermiry <erick_salas@ermiry.com>
2020-07-23 15:01:12 -0500

    Updated string type

--- include/client/types/string.h
+++ include/client/types/string.h
@@ -5,14 +5,20 @@
 
 typedef struct String {
 
-    unsigned int len;
-    char *str;
+	unsigned int len;
+	char *str;
 
 } String;
 
 extern String *str_new (const char *str);
-extern String *str_create (const char *format, ...);
+
 extern void str_delete (void *str_ptr);
+
+extern String *str_create (const char *format, ...);
+
+extern int str_compare (const String *s1, const String *s2);
+
+extern int str_comparator (const void *a, const void *b);
 
 extern void str_copy (String *to, String *from);
 
@@ -30,10 +36,8 @@ extern void str_append_char (String *s, const char c);
 extern void str_append_c_string (String *s, const char *c_str);
 
 extern void str_to_upper (String *string);
-extern void str_to_lower (String *string);
 
-extern int str_compare (const String *s1, const String *s2);
-extern int str_comparator (const void *a, const void *b) ;
+extern void str_to_lower (String *string);
 
 extern char **str_split (String *string, const char delim, int *n_tokens);
 
@@ -52,42 +56,42 @@ extern int str_contains (String *string, char *to_find);
 
 typedef enum SStringSize {
 
-    SS_SMALL = 64,
-    SS_MEDIUM = 128,
-    SS_LARGE = 256,
-    SS_EXTRA_LARGE = 512
+	SS_SMALL = 64,
+	SS_MEDIUM = 128,
+	SS_LARGE = 256,
+	SS_EXTRA_LARGE = 512
 
 } SStringSize;
 
 // serialized string (small)
 typedef struct SStringS {
 
-    u16 len;
-    char string[64];
+	u16 len;
+	char string[64];
 
 } SStringS;
 
 // serialized string (medium)
 typedef struct SStringM {
 
-    u16 len;
-    char string[128];
+	u16 len;
+	char string[128];
 
 } SStringM;
 
 // serialized string (large)
 typedef struct SStringL {
 
-    u16 len;
-    char string[256];
+	u16 len;
+	char string[256];
 
 } SStringL;
 
 // serialized string (extra large)
 typedef struct SStringXL {
 
-    u16 len;
-    char string[512];
+	u16 len;
+	char string[512];
 
 } SStringXL;
 
