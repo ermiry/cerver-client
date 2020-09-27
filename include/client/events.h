76ea7b2ef1eba987ab861b180cf0e7dc0ca9bd63
ermiry <erick_salas@ermiry.com>
2020-09-27 17:07:02 -0500

    Changed events source indentation from spaces to tabs

--- include/client/events.h
+++ include/client/events.h
@@ -13,85 +13,93 @@ struct _Connection;
 
 typedef enum ClientEventType {
 
-    CLIENT_EVENT_NONE                  = 0, 
+	CLIENT_EVENT_NONE                  = 0,
 
-    CLIENT_EVENT_CONNECTED,            // connected to cerver
-    CLIENT_EVENT_DISCONNECTED,         // disconnected from the cerver, either by the cerver or by losing connection
+	CLIENT_EVENT_CONNECTED,            // connected to cerver
+	CLIENT_EVENT_DISCONNECTED,         // disconnected from the cerver, either by the cerver or by losing connection
 
-    CLIENT_EVENT_CONNECTION_FAILED,    // failed to connect to cerver
-    CLIENT_EVENT_CONNECTION_CLOSE,     // this happens when a call to a recv () methods returns <= 0, the connection is clossed directly by client
+	CLIENT_EVENT_CONNECTION_FAILED,    // failed to connect to cerver
+	CLIENT_EVENT_CONNECTION_CLOSE,     // this happens when a call to a recv () methods returns <= 0, the connection is clossed directly by client
 
-    CLIENT_EVENT_CONNECTION_DATA,      // data has been received, only triggered from client request methods
+	CLIENT_EVENT_CONNECTION_DATA,      // data has been received, only triggered from client request methods
 
-    CLIENT_EVENT_CERVER_INFO,          // received cerver info from the cerver
-    CLIENT_EVENT_CERVER_TEARDOWN,      // the cerver is going to teardown (disconnect happens automatically)
-    CLIENT_EVENT_CERVER_STATS,         // received cerver stats
-    CLIENT_EVENT_CERVER_GAME_STATS,    // received cerver game stats
+	CLIENT_EVENT_CERVER_INFO,          // received cerver info from the cerver
+	CLIENT_EVENT_CERVER_TEARDOWN,      // the cerver is going to teardown (disconnect happens automatically)
+	CLIENT_EVENT_CERVER_STATS,         // received cerver stats
+	CLIENT_EVENT_CERVER_GAME_STATS,    // received cerver game stats
 
-    CLIENT_EVENT_AUTH_SENT,            // auth data has been sent to the cerver
-    CLIENT_EVENT_SUCCESS_AUTH,         // auth with cerver has been successfull
-    CLIENT_EVENT_MAX_AUTH_TRIES,       // maxed out attempts to authenticate to cerver, so try again
+	CLIENT_EVENT_AUTH_SENT,            // auth data has been sent to the cerver
+	CLIENT_EVENT_SUCCESS_AUTH,         // auth with cerver has been successfull
+	CLIENT_EVENT_MAX_AUTH_TRIES,       // maxed out attempts to authenticate to cerver, so try again
 
-    CLIENT_EVENT_LOBBY_CREATE,         // a new lobby was successfully created
-    CLIENT_EVENT_LOBBY_JOIN,           // correctly joined a new lobby
-    CLIENT_EVENT_LOBBY_LEAVE,          // successfully exited a lobby
+	CLIENT_EVENT_LOBBY_CREATE,         // a new lobby was successfully created
+	CLIENT_EVENT_LOBBY_JOIN,           // correctly joined a new lobby
+	CLIENT_EVENT_LOBBY_LEAVE,          // successfully exited a lobby
 
-    CLIENT_EVENT_LOBBY_START,          // the game in the lobby has started
+	CLIENT_EVENT_LOBBY_START,          // the game in the lobby has started
 
 } ClientEventType;
 
 typedef struct ClientEvent {
 
-    ClientEventType type;         // the event we are waiting to happen
-    bool create_thread;                 // create a detachable thread to run action
-    bool drop_after_trigger;            // if we only want to trigger the event once
+	ClientEventType type;         // the event we are waiting to happen
+	bool create_thread;                 // create a detachable thread to run action
+	bool drop_after_trigger;            // if we only want to trigger the event once
 
-    // the request that triggered the event
-    // this is usefull for custom events
-    u32 request_type; 
-    void *response_data;                // data that came with the response   
-    Action delete_response_data;       
+	// the request that triggered the event
+	// this is usefull for custom events
+	u32 request_type;
+	void *response_data;                // data that came with the response
+	Action delete_response_data;
 
-    Action action;                      // the action to be triggered
-    void *action_args;                  // the action arguments
-    Action delete_action_args;          // how to get rid of the data
+	Action action;                      // the action to be triggered
+	void *action_args;                  // the action arguments
+	Action delete_action_args;          // how to get rid of the data
 
 } ClientEvent;
 
 // registers an action to be triggered when the specified event occurs
 // if there is an existing action registered to an event, it will be overrided
-// a newly allocated ClientEventData structure will be passed to your method 
+// a newly allocated ClientEventData structure will be passed to your method
 // that should be free using the client_event_data_delete () method
 // returns 0 on success, 1 on error
-CLIENT_EXPORT u8 client_event_register (struct _Client *client, const ClientEventType event_type, 
-    Action action, void *action_args, Action delete_action_args, 
-    bool create_thread, bool drop_after_trigger);
+CLIENT_EXPORT u8 client_event_register (
+	struct _Client *client,
+	const ClientEventType event_type,
+	Action action, void *action_args, Action delete_action_args,
+	bool create_thread, bool drop_after_trigger
+);
 
 // unregister the action associated with an event
 // deletes the action args using the delete_action_args () if NOT NULL
 // returns 0 on success, 1 on error
 CLIENT_EXPORT u8 client_event_unregister (struct _Client *client, const ClientEventType event_type);
 
-CLIENT_PRIVATE void client_event_set_response (struct _Client *client, const ClientEventType event_type,
-    void *response_data, Action delete_response_data);
+CLIENT_PRIVATE void client_event_set_response (
+	struct _Client *client,
+	const ClientEventType event_type,
+	void *response_data, Action delete_response_data
+);
 
 // triggers all the actions that are registred to an event
-CLIENT_PRIVATE void client_event_trigger (const ClientEventType event_type,
-    const struct _Client *client, const struct _Connection *connection);
+CLIENT_PRIVATE void client_event_trigger (
+	const ClientEventType event_type,
+	const struct _Client *client, const struct _Connection *connection
+);
 
 #pragma region data
 
 // structure that is passed to the user registered method
 typedef struct ClientEventData {
 
-    const struct _Client *client;
-    const struct _Connection *connection;
+	const struct _Client *client;
+	const struct _Connection *connection;
 
-    void *response_data;                // data that came with the response   
-    Action delete_response_data;  
+	void *response_data;                // data that came with the response
+	Action delete_response_data;
 
-    void *action_args;                  // the action arguments
-    Action delete_action_args;
+	void *action_args;                  // the action arguments
+	Action delete_action_args;
 
 } ClientEventData;
 
--- src/client/events.c
+++ src/client/events.c
@@ -18,44 +18,46 @@ u8 client_event_unregister (Client *client, ClientEventType event_type);
 
 static ClientEventData *client_event_data_new (void) {
 
-    ClientEventData *event_data = (ClientEventData *) malloc (sizeof (ClientEventData));
-    if (event_data) {
-        event_data->client = NULL;
-        event_data->connection = NULL;
+	ClientEventData *event_data = (ClientEventData *) malloc (sizeof (ClientEventData));
+	if (event_data) {
+		event_data->client = NULL;
+		event_data->connection = NULL;
 
-        event_data->response_data = NULL;
-        event_data->delete_response_data = NULL;
+		event_data->response_data = NULL;
+		event_data->delete_response_data = NULL;
 
-        event_data->action_args = NULL;
-        event_data->delete_action_args = NULL;
-    }
+		event_data->action_args = NULL;
+		event_data->delete_action_args = NULL;
+	}
 
-    return event_data;
+	return event_data;
 
 }
 
 void client_event_data_delete (ClientEventData *event_data) {
 
-    if (event_data) free (event_data);
+	if (event_data) free (event_data);
 
 }
 
-static ClientEventData *client_event_data_create (const Client *client, const Connection *connection, 
-    ClientEvent *event) {
+static ClientEventData *client_event_data_create (
+	const Client *client, const Connection *connection,
+	ClientEvent *event
+) {
 
-    ClientEventData *event_data = client_event_data_new ();
-    if (event_data) {
-        event_data->client = client;
-        event_data->connection = connection;
+	ClientEventData *event_data = client_event_data_new ();
+	if (event_data) {
+		event_data->client = client;
+		event_data->connection = connection;
 
-        event_data->response_data = event->response_data;
-        event_data->delete_response_data = event->delete_response_data;
+		event_data->response_data = event->response_data;
+		event_data->delete_response_data = event->delete_response_data;
 
-        event_data->action_args = event->action_args;
-        event_data->delete_action_args = event->delete_action_args;
-    }
+		event_data->action_args = event->action_args;
+		event_data->delete_action_args = event->delete_action_args;
+	}
 
-    return event_data;
+	return event_data;
 
 }
 
@@ -65,115 +67,120 @@ static ClientEventData *client_event_data_create (const Client *client, const Co
 
 static ClientEvent *client_event_new (void) {
 
-    ClientEvent *event = (ClientEvent *) malloc (sizeof (ClientEvent));
-    if (event) {
-        event->type = CLIENT_EVENT_NONE;
+	ClientEvent *event = (ClientEvent *) malloc (sizeof (ClientEvent));
+	if (event) {
+		event->type = CLIENT_EVENT_NONE;
 
-        event->create_thread = false;
-        event->drop_after_trigger = false;
+		event->create_thread = false;
+		event->drop_after_trigger = false;
 
-        event->request_type = 0;
-        event->response_data = NULL;
-        event->delete_response_data = NULL;
+		event->request_type = 0;
+		event->response_data = NULL;
+		event->delete_response_data = NULL;
 
-        event->action = NULL;
-        event->action_args = NULL;
-        event->delete_action_args = NULL;
-    }
+		event->action = NULL;
+		event->action_args = NULL;
+		event->delete_action_args = NULL;
+	}
 
-    return event;
+	return event;
 
 }
 
 static void client_event_delete (void *ptr) {
 
-    if (ptr) {
-        ClientEvent *event = (ClientEvent *) ptr;
+	if (ptr) {
+		ClientEvent *event = (ClientEvent *) ptr;
 
-        if (event->response_data) {
-            if (event->delete_response_data) 
-                event->delete_response_data (event->response_data);
-        }
-        
-        if (event->action_args) {
-            if (event->delete_action_args)
-                event->delete_action_args (event->action_args);
-        }
+		if (event->response_data) {
+			if (event->delete_response_data)
+				event->delete_response_data (event->response_data);
+		}
 
-        free (event);
-    }
+		if (event->action_args) {
+			if (event->delete_action_args)
+				event->delete_action_args (event->action_args);
+		}
+
+		free (event);
+	}
 
 }
 
-static ClientEvent *client_event_get (const Client *client, const ClientEventType event_type, 
-    ListElement **le_ptr) {
+static ClientEvent *client_event_get (
+	const Client *client, const ClientEventType event_type,
+	ListElement **le_ptr
+) {
 
-    if (client) {
-        if (client->registered_events) {
-            ClientEvent *event = NULL;
-            for (ListElement *le = dlist_start (client->registered_events); le; le = le->next) {
-                event = (ClientEvent *) le->data;
-                if (event->type == event_type) {
-                    if (le_ptr) *le_ptr = le;
-                    return event;
-                } 
-            }
-        }
-    }
+	if (client) {
+		if (client->registered_events) {
+			ClientEvent *event = NULL;
+			for (ListElement *le = dlist_start (client->registered_events); le; le = le->next) {
+				event = (ClientEvent *) le->data;
+				if (event->type == event_type) {
+					if (le_ptr) *le_ptr = le;
+					return event;
+				}
+			}
+		}
+	}
 
-    return NULL;
+	return NULL;
 
 }
 
 static void client_event_pop (DoubleList *list, ListElement *le) {
 
-    if (le) {
-        void *data = dlist_remove_element (list, le);
-        if (data) client_event_delete (data);
-    }
+	if (le) {
+		void *data = dlist_remove_element (list, le);
+		if (data) client_event_delete (data);
+	}
 
 }
 
 // registers an action to be triggered when the specified event occurs
 // if there is an existing action registered to an event, it will be overrided
-// a newly allocated ClientEventData structure will be passed to your method 
+// a newly allocated ClientEventData structure will be passed to your method
 // that should be free using the client_event_data_delete () method
 // returns 0 on success, 1 on error
-u8 client_event_register (Client *client, const ClientEventType event_type, 
-    Action action, void *action_args, Action delete_action_args, 
-    bool create_thread, bool drop_after_trigger) {
+u8 client_event_register (
+	Client *client,
+	const ClientEventType event_type,
+	Action action, void *action_args, Action delete_action_args,
+	bool create_thread, bool drop_after_trigger
+) {
 
-    u8 retval = 1;
+	u8 retval = 1;
 
-    if (client) {
-        if (client->registered_events) {
-            ClientEvent *event = client_event_new ();
-            if (event) {
-                event->type = event_type;
+	if (client) {
+		if (client->registered_events) {
+			ClientEvent *event = client_event_new ();
+			if (event) {
+				event->type = event_type;
 
-                event->create_thread = create_thread;
-                event->drop_after_trigger = drop_after_trigger;
+				event->create_thread = create_thread;
+				event->drop_after_trigger = drop_after_trigger;
 
-                event->action = action;
-                event->action_args = action_args;
-                event->delete_action_args = delete_action_args;
+				event->action = action;
+				event->action_args = action_args;
+				event->delete_action_args = delete_action_args;
 
-                // search if there is an action already registred for that event and remove it
-                (void) client_event_unregister (client, event_type);
+				// search if there is an action already registred for that event and remove it
+				(void) client_event_unregister (client, event_type);
 
-                if (!dlist_insert_after (
-                    client->registered_events, 
-                    dlist_end (client->registered_events),
-                    event
-                )) {
-                    retval = 0;
-                }
-            }
-        }
-    }
+				if (!dlist_insert_after (
+					client->registered_events,
+					dlist_end (client->registered_events),
+					event
+				)) {
+					retval = 0;
+				}
+			}
+		}
+	}
 
-    return retval;
-    
+	return retval;
+
 }
 
 // unregister the action associated with an event
@@ -181,73 +188,78 @@ u8 client_event_register (Client *client, const ClientEventType event_type,
 // returns 0 on success, 1 on error
 u8 client_event_unregister (Client *client, const ClientEventType event_type) {
 
-    u8 retval = 1;
+	u8 retval = 1;
 
-    if (client) {
-        if (client->registered_events) {
-            ClientEvent *event = NULL;
-            for (ListElement *le = dlist_start (client->registered_events); le; le = le->next) {
-                event = (ClientEvent *) le->data;
-                if (event->type == event_type) {
-                    client_event_delete (dlist_remove_element (client->registered_events, le));
-                    retval = 0;
+	if (client) {
+		if (client->registered_events) {
+			ClientEvent *event = NULL;
+			for (ListElement *le = dlist_start (client->registered_events); le; le = le->next) {
+				event = (ClientEvent *) le->data;
+				if (event->type == event_type) {
+					client_event_delete (dlist_remove_element (client->registered_events, le));
+					retval = 0;
 
-                    break;
-                }
-            }
-        }
-    }
+					break;
+				}
+			}
+		}
+	}
 
-    return retval;
+	return retval;
 
 }
 
-void client_event_set_response (Client *client, const ClientEventType event_type,
-    void *response_data, Action delete_response_data) {
+void client_event_set_response (
+	Client *client,
+	const ClientEventType event_type,
+	void *response_data, Action delete_response_data
+) {
 
-    if (client) {
-        ClientEvent *event = client_event_get (client, event_type, NULL);
-        if (event) {
-            event->response_data = response_data;
-            event->delete_response_data = delete_response_data;
-        }
-    }
+	if (client) {
+		ClientEvent *event = client_event_get (client, event_type, NULL);
+		if (event) {
+			event->response_data = response_data;
+			event->delete_response_data = delete_response_data;
+		}
+	}
 
-} 
+}
 
 // triggers all the actions that are registred to an event
-void client_event_trigger (const ClientEventType event_type, 
-    const Client *client, const Connection *connection) {
+void client_event_trigger (
+	const ClientEventType event_type,
+	const Client *client, const Connection *connection
+) {
 
-    if (client) {
-        ListElement *le = NULL;
-        ClientEvent *event = client_event_get (client, event_type, &le);
-        if (event) {
-            // trigger the action
-            if (event->action) {
-                if (event->create_thread) {
-                    pthread_t thread_id = 0;
-                    thread_create_detachable (
-                        &thread_id,
-                        (void *(*)(void *)) event->action, 
-                        client_event_data_create (
-                            client, connection,
-                            event
-                        )
-                    );
-                }
+	if (client) {
+		ListElement *le = NULL;
+		ClientEvent *event = client_event_get (client, event_type, &le);
+		if (event) {
+			// trigger the action
+			if (event->action) {
+				if (event->create_thread) {
+					pthread_t thread_id = 0;
+					thread_create_detachable (
+						&thread_id,
+						(void *(*)(void *)) event->action,
+						client_event_data_create (
+							client, connection,
+							event
+						)
+					);
+				}
 
-                else {
-                    event->action (client_event_data_create (
-                        client, connection, 
-                        event
-                    ));
-                }
-                
-                if (event->drop_after_trigger) client_event_pop (client->registered_events, le);
-            }
-        }
-    }
+				else {
+					event->action (client_event_data_create (
+						client, connection,
+						event
+					));
+				}
+
+				if (event->drop_after_trigger) client_event_pop (client->registered_events, le);
+			}
+		}
+	}
 
 }
 
@@ -257,20 +269,20 @@ void client_event_trigger (const ClientEventType event_type,
 
 u8 client_events_init (Client *client) {
 
-    u8 retval = 1;
+	u8 retval = 1;
 
-    if (client) {
-        client->registered_events = dlist_init (client_event_delete, NULL);
-        retval = client->registered_events ? 0 : 1;
-    }
+	if (client) {
+		client->registered_events = dlist_init (client_event_delete, NULL);
+		retval = client->registered_events ? 0 : 1;
+	}
 
-    return retval;
+	return retval;
 
 }
 
-void client_events_end (Client *client) { 
+void client_events_end (Client *client) {
 
-    if (client) dlist_delete (client->registered_events);
+	if (client) dlist_delete (client->registered_events);
 
 }
 
