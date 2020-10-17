e51ee1e96979303ace2168d496f7734e1c163886
ermiry <erick_salas@ermiry.com>
2020-10-16 23:31:46 -0500

    Updated version to 1.3.2rc-1

--- include/client/version.h
+++ include/client/version.h
@@ -3,10 +3,10 @@
 
 #include "client/config.h"
 
-#define CLIENT_VERSION                      "1.3.1rc-9"
-#define CLIENT_VERSION_NAME                 "Release 1.3.1rc-9"
-#define CLIENT_VERSION_DATE			        "11/10/2020"
-#define CLIENT_VERSION_TIME			        "23:29 CST"
+#define CLIENT_VERSION                      "1.3.2rc-1"
+#define CLIENT_VERSION_NAME                 "Release 1.3.2rc-1"
+#define CLIENT_VERSION_DATE			        "16/10/2020"
+#define CLIENT_VERSION_TIME			        "23:31 CST"
 #define CLIENT_VERSION_AUTHOR			    "Erick Salas"
 
 // print full cerver client version information
