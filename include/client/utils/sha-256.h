597e2e2a26a9ac3fc321cfd877fb933e194f6ca6
ermiry <erick.salas@ermiry.com>
2021-01-12 23:53:25 -0600

    Updated sha256 sources with latest methods

--- include/client/utils/sha-256.h
+++ include/client/utils/sha-256.h
@@ -1,13 +1,39 @@
-#ifndef _CLIENT_UTILS_SHA_256_H_
-#define _CLIENT_UTILS_SHA_256_H_
+#ifndef _CLIENT_UTILS_SHA256_H_
+#define _CLIENT_UTILS_SHA256_H_
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #include "client/config.h"
 
-CLIENT_PUBLIC void sha_256_calc (uint8_t hash[32], const void *input, size_t len);
+#define SHA256_STRING_LEN		80
 
-CLIENT_PUBLIC void sha_256_hash_to_string (char string[65], const uint8_t hash[32]);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+CLIENT_PUBLIC void sha256_calc (
+	uint8_t hash[32], const void *input, size_t len
+);
+
+CLIENT_PUBLIC void sha256_hash_to_string (
+	char string[], const uint8_t hash[32]
+);
+
+// generates a sha256 string from the input and places it in output
+// output must be at least 65 bytes long
+CLIENT_PUBLIC void sha256_generate (
+	char *output, const char *input, const size_t inlen
+);
+
+// generates a sha256 string from the input
+// returns a newly allocated string
+CLIENT_PUBLIC char *sha256_generate_output (
+	const char *input, const size_t inlen
+);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
