460d9489802f90fe941bbe4b73fb234da3f96434
ermiry <erick.salas@ermiry.com>
2021-04-09 12:38:31 -0500

    Refactored run tests scripts instructions
    Also handling json methods unit tests

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=bin ./test/bin/collections --quiet || { exit 1; }
+./test/bin/collections --quiet || { exit 1; }
 
-LD_LIBRARY_PATH=bin ./test/bin/utils || { exit 1; }
+./test/bin/utils || { exit 1; }
+
+./test/bin/json || { exit 1; }
